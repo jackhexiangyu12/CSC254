ffd3bbf008cdae4577463218c89bf0c30760cc14
Gao Fan <gfan@cycle1.csug.rochester.edu>
2019-09-15 16:00:17 -0400

    finish Python

--- A1_comparing_languages/Python/trees.py
+++ A1_comparing_languages/Python/trees.py
@@ -3,18 +3,23 @@ import sys
 def gen(n):
     result = []
 
+    # When N is 0
     tmp = ['']
     result.append(tmp)
 
     if n > 0:
+        # When N is 1
         tmp = ['(.)']
         result.append(tmp)
 
+        # When N is greater than 1
         for i in range(2,n+1):
             tmp = []
+            # For all possible leftsubtree with j nodes and rightsubtree with i-j-1 nodes
             for j in reversed(range(i)):
                 for left in result[j]:
                     for right in result[i-j-1]:
+                        # Add leftsubtree and rightsubtree to root and store in list
                         tmp.append('(' + left + '.' + right + ')')
             result.append(tmp)
         
