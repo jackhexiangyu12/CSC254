95b357a31dd1b7c8065206e6b45eb85703c71891
c25423 <c25423@gmail.com>
2019-09-24 15:37:26 -0400

    add enum

--- A2-syntax-error-recovery/scan.h
+++ A2-syntax-error-recovery/scan.h
@@ -2,8 +2,9 @@
     Michael L. Scott, 2008-2019.
 */
 
-typedef enum {t_read, t_write, t_id, t_literal, t_gets,
-                t_add, t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof} token;
+enum token {t_read, t_write, t_id, t_literal, t_gets,
+                t_add, t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof,
+                t_if, t_while, t_end, t_eq, t_neq, t_gt, t_lt, t_gte, t_ste};
 
 #define MAX_TOKEN_LEN 100
 extern char token_image[MAX_TOKEN_LEN];
