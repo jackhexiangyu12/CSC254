0fd6ea52b9d0d83b19709e2522b779418d077a22
c25423 <c25423@gmail.com>
2019-09-24 15:37:36 -0400

    delete printf

--- A2-syntax-error-recovery/parse.cpp
+++ A2-syntax-error-recovery/parse.cpp
@@ -22,19 +22,15 @@ const char* names[] = {"read", "write", "id", "literal", "gets",
 static token input_token;
 
 void error () {
-    // printf ("syntax error\n");
     cout << "syntax error" << endl;
     exit (1);
 }
 
 void match (token expected) {
     if (input_token == expected) {
-        // printf ("matched %s", names[input_token]);
         cout << "matched " << names[input_token] << endl;
         if (input_token == t_id || input_token == t_literal)
-            // printf (": %s", token_image);
             cout << ": " << token_image << endl;
-        // printf ("\n");
         cout << endl;
         input_token = scan ();
     }
@@ -58,7 +54,6 @@ void program () {
         case t_read:
         case t_write:
         case t_eof:
-            // printf ("predict program --> stmt_list eof\n");
             cout << "predict program --> stmt_list eof" << endl;
             stmt_list ();
             match (t_eof);
@@ -72,13 +67,11 @@ void stmt_list () {
         case t_id:
         case t_read:
         case t_write:
-            // printf ("predict stmt_list --> stmt stmt_list\n");
             cout << "predict stmt_list --> stmt stmt_list" << endl;
             stmt ();
             stmt_list ();
             break;
         case t_eof:
-            // printf ("predict stmt_list --> epsilon\n");
             cout << "predict stmt_list --> epsilon" << endl;
             break;          /*  epsilon production */
         default: error ();
@@ -88,20 +81,17 @@ void stmt_list () {
 void stmt () {
     switch (input_token) {
         case t_id:
-            // printf ("predict stmt --> id gets expr\n");
             cout << "predict stmt --> id gets expr" << endl;
             match (t_id);
             match (t_gets);
             expr ();
             break;
         case t_read:
-            // printf ("predict stmt --> read id\n");
             cout << "predict stmt --> read id" << endl;
             match (t_read);
             match (t_id);
             break;
         case t_write:
-            // printf ("predict stmt --> write expr\n");
             cout << "predict stmt --> write expr" << endl;
             match (t_write);
             expr ();
@@ -115,7 +105,6 @@ void expr () {
         case t_id:
         case t_literal:
         case t_lparen:
-            // printf ("predict expr --> term term_tail\n");
             cout << "predict expr --> term term_tail" << endl;
             term ();
             term_tail ();
@@ -128,7 +117,6 @@ void term_tail () {
     switch (input_token) {
         case t_add:
         case t_sub:
-            // printf ("predict term_tail --> add_op term term_tail\n");
             cout << "predict term_tail --> add_op term term_tail" << endl;
             add_op ();
             term ();
@@ -139,7 +127,6 @@ void term_tail () {
         case t_read:
         case t_write:
         case t_eof:
-            // printf ("predict term_tail --> epsilon\n");
             cout << "predict term_tail --> epsilon" << endl;
             break;          /*  epsilon production */
         default: error ();
@@ -151,7 +138,6 @@ void term () {
         case t_id:
         case t_literal:
         case t_lparen:
-            // printf ("predict term --> factor factor_tail\n");
             cout << "predict term --> factor factor_tail" << endl;
             factor ();
             factor_tail ();
@@ -164,7 +150,6 @@ void factor_tail () {
     switch (input_token) {
         case t_mul:
         case t_div:
-            // printf ("predict factor_tail --> mul_op factor factor_tail\n");
             cout << "predict factor_tail --> mul_op factor factor_tail" << endl;
             mul_op ();
             factor ();
@@ -177,7 +162,6 @@ void factor_tail () {
         case t_read:
         case t_write:
         case t_eof:
-            // printf ("predict factor_tail --> epsilon\n");
             cout << "predict factor_tail --> epsilon" << endl;
             break;          /*  epsilon production */
         default: error ();
@@ -187,17 +171,14 @@ void factor_tail () {
 void factor () {
     switch (input_token) {
         case t_id :
-            // printf ("predict factor --> id\n");
             cout << "predict factor --> id" << endl;
             match (t_id);
             break;
         case t_literal:
-            // printf ("predict factor --> literal\n");
             cout << "predict factor --> literal" << endl;
             match (t_literal);
             break;
         case t_lparen:
-            // printf ("predict factor --> lparen expr rparen\n");
             cout << "predict factor --> lparen expr rparen" << endl;
             match (t_lparen);
             expr ();
@@ -210,12 +191,10 @@ void factor () {
 void add_op () {
     switch (input_token) {
         case t_add:
-            // printf ("predict add_op --> add\n");
             cout << "predict add_op --> add" << endl;
             match (t_add);
             break;
         case t_sub:
-            // printf ("predict add_op --> sub\n");
             cout << "predict add_op --> sub" << endl;
             match (t_sub);
             break;
@@ -226,12 +205,10 @@ void add_op () {
 void mul_op () {
     switch (input_token) {
         case t_mul:
-            // printf ("predict mul_op --> mul\n");
             cout << "predict mul_op --> mul" << endl;
             match (t_mul);
             break;
         case t_div:
-            // printf ("predict mul_op --> div\n");
             cout << "predict mul_op --> div" << endl;
             match (t_div);
             break;
