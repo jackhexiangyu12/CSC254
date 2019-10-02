1157f8a4d5844d07c98595f0b657942e092a4ddb
c25423 <c25423@gmail.com>
2019-10-02 01:30:36 -0400

    reset

--- A3/interpreter.ml
+++ A3/interpreter.ml
@@ -1,5 +1,3 @@
-#load "str.cma";;
-
 (*******************************************************************
     LL(1) parser generator, syntax tree builder for an extended
     calculator language, and (skeleton of an) interpreter for the
