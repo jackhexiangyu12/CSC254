b330ad68947c1dfed50e3585837acf777f89516d
c25423 <c25423@gmail.com>
2019-11-02 15:32:41 -0400

    store result

---
+++ A4/xref.rb
@@ -0,0 +1,18 @@
+require 'open3'
+
+# Get args
+
+if ARGV.length < 1
+    puts "Error: Missing executable name. "
+end
+
+exec_name = ARGV[0]
+
+# Set regex for objdump and dwarfdump
+
+# Get results from objdump and dwarfdump
+
+obj_raw, stdeerr, status = Open3.capture3("objdump -d " + exec_name)
+File.open("objdump.txt", 'w') { |f| f.write(obj_raw) }
+dwarf_raw, stdeerr, status = Open3.capture3("llvm-dwarfdump --debug-line " + exec_name)
+File.open("dwarfdump.txt", 'w') { |f| f.write(dwarf_raw) }
