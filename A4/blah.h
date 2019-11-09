045c8c9ca56d92a5906b54e73ffb218397a6ae78
c25423 <c25423@gmail.com>
2019-11-08 19:47:43 -0500

    rm examples

--- A4/blah.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define NO_INLINE __attribute__((noinline))
-
-inline int blah() {
-    int x;
-    scanf("%d", &x);
-    return x;
-}
--- A4/fib.c
+++
@@ -1,9 +0,0 @@
-#include "fib.h"
-
-int fib(int a) {
-    if (a < 2) {
-         return a;
-    } else {
-        return fib(a-2) + fib(a-1);
-    }
-}
