88aef9eae62a6997486550a36058d1f5da2d5650
c25423 <c25423@gmail.com>
2019-12-13 21:33:53 -0500

    add main

---
+++ A6/main.cc
@@ -0,0 +1,56 @@
+#include "set.hpp"
+
+int main() {
+
+    // Some miscellaneous code to get you started on testing your sets.
+    // The following should work:
+
+    std_simple_set<int> R;
+    R += 3;
+    cout << "3 is " << (R.contains(3) ? "" : "not ") << "in R\n";
+    cout << "5 is " << (R.contains(5) ? "" : "not ") << "in R\n";
+
+    simple_set<double>* S = new std_simple_set<double>();
+    *S += 3.14;
+    cout << "pi is " << (S->contains(3.14) ? "" : "not ") << "in S\n";
+    cout << "e is " << (S->contains(2.718) ? "" : "not ") << "in S\n";
+
+    std_simple_set<string> U;
+    U += "hello";
+    cout << "\"hello\" is " << (U.contains("hello") ? "" : "not ") << "in U\n";
+    cout << "\"foo\" is " << (U.contains("foo") ? "" : "not ") << "in U\n";
+
+    range<string> r1("a", true, "f", true);
+    cout << "\"b\" is " << (r1.contains("b") ? "" : "not ") << "in r1\n";
+    cout << "\"aaa\" is " << (r1.contains("aaa") ? "" : "not ") << "in r1\n";
+    cout << "\"faa\" is " << (r1.contains("faa") ? "" : "not ") << "in r1\n";
+
+    range<const char*, lexico_less> r2("a", true, "f", true);
+    cout << "\"b\" is " << (r2.contains("b") ? "" : "not ") << "in r2\n";
+    cout << "\"aaa\" is " << (r2.contains("aaa") ? "" : "not ") << "in r2\n";
+    cout << "\"faa\" is " << (r2.contains("faa") ? "" : "not ") << "in r2\n";
+
+    // The following will not work correctly yet:
+
+    range_set<int>* X = new std_range_set<int>();
+    *X += range<int>(5, true, 8, false);
+    if (X->contains(4)) cout << "4 is in X\n";
+    if (X->contains(5)) cout << "5 is in X\n";      // should print
+    if (X->contains(6)) cout << "6 is in X\n";      // should print
+    if (X->contains(7)) cout << "7 is in X\n";      // should print
+    if (X->contains(8)) cout << "8 is in X\n";
+    if (X->contains(9)) cout << "9 is in X\n";
+    *X -= range<int>(6, true, 10, false);
+    if (X->contains(4)) cout << "4 is now in X\n";
+    if (X->contains(5)) cout << "5 is now in X\n";      // should print
+    if (X->contains(6)) cout << "6 is now in X\n";
+    if (X->contains(7)) cout << "7 is now in X\n";
+    if (X->contains(8)) cout << "8 is now in X\n";
+    if (X->contains(9)) cout << "9 is now in X\n";
+
+    simple_set<weekday>* V = new carray_simple_set<weekday>(mon, (weekday)5);
+
+    hashed_simple_set<int, cast_to_int<int>> H(100);
+
+    // bin_search_simple_set<double> J(100);
+}
